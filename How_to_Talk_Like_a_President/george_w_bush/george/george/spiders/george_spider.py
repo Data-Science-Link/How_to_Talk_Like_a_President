1b52db58823034c36281872b15544eeb2976796c
Michael Link <attemptingtruth@gmail.com>
2020-05-07 16:45:26 -0400

    made updates to ensure URLs worked

--- How_to_Talk_Like_a_President/george_w_bush/george/george/spiders/george_spider.py
+++ How_to_Talk_Like_a_President/george_w_bush/george/george/spiders/george_spider.py
@@ -4,23 +4,34 @@ from george.items import GeorgeItem
 import re
 import math
 
-class BestBuySpider(Spider):
+class GeorgeSpider(Spider):
     name = 'george_spider'
     allowed_domains = ['georgewbush-whitehouse.archives.gov']
     start_urls = ['https://georgewbush-whitehouse.archives.gov/news/briefings/']
     
-    def parse(self, response):
-    # Assemble a list of the briefing urls, titles, and dates
+    def parse(self, response): # Assemble a list of the briefing urls, titles, and dates
+    
+    #Assembling all the a-href urls for the individual briefings
         main_path = 'https://georgewbush-whitehouse.archives.gov/news/briefings/'
-        left_path_v1 = main_path.split('/news/briefings/')[0]
-        right_path = response.xpath('//table//td/a/@href').extract()
-        # List comprehension to construct all the urls
-        briefing_urls = [left_path_v1 + str(paths) for paths in right_path]
+        left_path = main_path.split('/news/briefings/')[0]
+        right_path_raw = response.xpath('//table//td/a/@href').extract()
+        right_path = []
+        #the a href format changes. Need to conditionally format right path portion
+        for i in range(len(right_path_raw)):
+            if len(right_path_raw[i])<19:
+                right_path.append('/news/releases' + str(right_path_raw[i][1:]))
+            else:
+                right_path.append(right_path_raw[i])
+        #compiling the left and right sides for a complete URL
+        briefing_urls = [left_path + str(paths) for paths in right_path]
 
+    #Assembling the titles for each press briefing
         titles = response.xpath('//table//td/a/text()').extract()
+        
+    #Assembiling the dates of each briefing
         date_text = response.xpath('//table//td[@class="archive-date-cell"]').extract()
         date = []
-
+        #If two or more press briefings were held on the same day the date was not appended to the 2nd and 3rd. For loops account for this.
         for day in date_text:
             s = re.search('<td class="archive-date-cell">(.*)<br></td>', day).group(1)
             date.append(s)
@@ -29,7 +40,7 @@ class BestBuySpider(Spider):
             if date[day] == '':
                 date[day] = date[day-1]
 
-
+    #Writing all gathered data to csv
         item = GeorgeItem()
         for i in range(len(briefing_urls)):
             item['title'] = titles[i]
